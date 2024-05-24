0671917c81bb6cbc23730ccbc1f686aed6560591
nelsonmic <iamnelsonmichael@gmail.com>
2024-05-24 14:37:04 +0100

    feat(Profile screen): added delete account dialog

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -4,4 +4,5 @@ export { default as Chat } from "./Chat";
 export { default as Edit } from "./Edit";
 export { default as Google } from "./Google";
 export { default as Home } from "./Home";
+export { default as Trash } from "./Trash";
 export { default as User } from "./User";
