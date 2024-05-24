6a092f1ac4e538df0a43d76e346547c4405557c7
nelsonmic <iamnelsonmichael@gmail.com>
2024-05-24 10:41:46 +0100

    feat: complete auth screen

---
+++ src/app/auth/forgot-password/page.tsx
@@ -0,0 +1,11 @@
+import { P } from "@/components/atoms"
+
+const Page = () => {
+      return (
+            <div className="flex-1 flex items-center justify-center h-full">
+                  <P>Forgot Password screen</P>
+            </div>
+      )
+}
+
+export default Page;
