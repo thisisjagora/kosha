24193d47601fe2f456ff7f4c1d5d1a55380c4569
nelsonmic <iamnelsonmichael@gmail.com>
2024-05-26 04:08:59 +0100

    wip: complete tabs for sequence screens

--- src/app/(sequences)/hire-labour/page.tsx
+++ src/app/(sequences)/hire-labour/page.tsx
@@ -1,6 +1,37 @@
+"use client";
+import { Tabs, TabsContent, TabsCount, TabsList, TabsTrigger } from "@/components/tabs";
+import { useState } from "react";
+
 const Page = () => {
+      const [activeTab, setActiveTab] = useState<string>("dlt");
       return (
-            <div>Hire Labour</div>
+            <div>
+                  <Tabs value={activeTab} onValueChange={setActiveTab}>
+                        <TabsList>
+                              <TabsTrigger className="flex gap-2" value="dlt">
+                                    <TabsCount count="1" isActive={activeTab === "dlt"} />
+                                    Date, Location & Time
+                              </TabsTrigger>
+                              <TabsTrigger className="flex gap-2" value="itm" >
+                                    <TabsCount count="2" isActive={activeTab === "itm"} />
+                                    Items to Move
+                              </TabsTrigger>
+                              <TabsTrigger className="flex gap-2" value="generalInfo">
+                                    <TabsCount count="3" isActive={activeTab === "generalInfo"} />
+                                    General Info
+                              </TabsTrigger>
+                        </TabsList>
+                        <TabsContent value="dlt">
+                              <p>Date, Location & Time</p>
+                        </TabsContent>
+                        <TabsContent value="itm">
+                              <p>Items to move</p>
+                        </TabsContent>
+                        <TabsContent value="generalInfo">
+                              <p>General Info</p>
+                        </TabsContent>
+                  </Tabs>
+            </div>
       )
 }
 
