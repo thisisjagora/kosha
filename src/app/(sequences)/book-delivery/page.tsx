c5cf6705dfbda9d50aee550a044f252e6083b9f1
nelsonmic <iamnelsonmichael@gmail.com>
2024-05-26 04:08:59 +0100

    wip: complete tabs for sequence screens

--- src/app/(sequences)/book-delivery/page.tsx
+++ src/app/(sequences)/book-delivery/page.tsx
@@ -1,6 +1,37 @@
+"use client";
+import { Tabs, TabsContent, TabsCount, TabsList, TabsTrigger } from "@/components/tabs";
+import { useState } from "react";
+
 const Page = () => {
+      const [activeTab, setActiveTab] = useState<string>("dlt");
       return (
-            <div>Book Delivery</div>
+            <div>
+                  <Tabs value={activeTab} onValueChange={setActiveTab}>
+                        <TabsList>
+                              <TabsTrigger className="flex gap-2" value="dlt">
+                                    <TabsCount count="1" isActive={activeTab === "dlt"} />
+                                    Date, Location & Time
+                              </TabsTrigger>
+                              <TabsTrigger className="flex gap-2" value="pld" >
+                                    <TabsCount count="2" isActive={activeTab === "pld"} />
+                                    Pickup Location Details
+                              </TabsTrigger>
+                              <TabsTrigger className="flex gap-2" value="itu">
+                                    <TabsCount count="3" isActive={activeTab === "itu"} />
+                                    Items to Pick up
+                              </TabsTrigger>
+                        </TabsList>
+                        <TabsContent value="dlt">
+                              <p>Date, Location & Time</p>
+                        </TabsContent>
+                        <TabsContent value="pld">
+                              <p>Pickup Location Details</p>
+                        </TabsContent>
+                        <TabsContent value="itu">
+                              <p>Items to Pick up</p>
+                        </TabsContent>
+                  </Tabs>
+            </div>
       )
 }
 
--- src/app/(sequences)/layout.tsx
+++ src/app/(sequences)/layout.tsx
@@ -2,8 +2,10 @@ import { FC, PropsWithChildren } from "react"
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
       return (
-            <div>
-                  {children}
+            <div className="w-full h-full flex justify-center">
+                  <div className="flex-1 h-full max-w-[900px]">
+                        {children}
+                  </div>
             </div>
       )
 }
