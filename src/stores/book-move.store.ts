5f2440d08ab5ef635854ae348e69e60fb89d5746
nelsonmic <iamnelsonmichael@gmail.com>
2024-05-29 00:53:20 +0100

    wip: complete step 3 book move sequence

--- src/stores/book-move.store.ts
+++ src/stores/book-move.store.ts
@@ -4,6 +4,9 @@ import { create } from 'zustand';
 interface Store {
   formData: BookMove;
   update: (newData: Partial<BookMove>) => void;
+  updateField: <K extends keyof BookMove>(fieldName: K, newValue: BookMove[K]) => void;
+  removeStop: (index: number) => void;
+  removeImage: (index: number) => void;
 }
 
 const initialState: BookMove = {
@@ -13,12 +16,7 @@ const initialState: BookMove = {
             location: "",
             apartment: ""
       },
-      stops: [
-            {
-                  location: "",
-                  apartment: ""
-            }
-      ],
+      stops: [],
       finalDestination: {
             location: "",
             apartment: ""
@@ -33,20 +31,38 @@ const initialState: BookMove = {
             flightOfStairs: "",
             buildingType: ""
       },
-      PUDStops: [
-            {
-                  elevatorAccess: "",
-                  flightOfStairs: "",
-                  buildingType: ""
-            }
-      ]
+      PUDStops: [],
+      majorAppliances: "",
+      workOutEquipment: "",
+      pianos:"",
+      hotTubs: "",
+      poolTables: "",
+      numberOfBoxes: "",
+      instructions: "",
+      images: []
 }
 
 const useBookMoveStore = create<Store>((set) => ({
   formData: initialState,
   update: (newData) => set((state) => ({
     formData: { ...state.formData, ...newData }
-  }))
+  })),
+  updateField: (fieldName, newValue) => set((state) => ({
+      formData: { ...state.formData, [fieldName]: newValue }
+    })),
+  removeStop: (index) => set((state) => ({
+      formData: {
+        ...state.formData,
+        stops: state.formData.stops.filter((_, i) => i !== index),
+        PUDStops: state.formData.PUDStops?.filter((_, i) => i !== index)
+      }
+    })),
+    removeImage: (index) => set((state) => {
+      const newImages = state.formData.images.filter((_, i) => i !== index);
+      return {
+        formData: { ...state.formData, images: newImages }
+      };
+    })
 }));
 
 export default useBookMoveStore;
