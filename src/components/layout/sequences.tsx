70445df789c2be6ef05ba1d276470114f0612930
nelsonmic <iamnelsonmichael@gmail.com>
2024-06-08 17:28:30 +0100

    wip: added general quotations + quotes comps

---
+++ src/components/layout/sequences.tsx
@@ -0,0 +1,3 @@
+import { FC, PropsWithChildren } from "react"
+
+export const SequencesLayout:FC<PropsWithChildren> = ({...props}) => <div {...props} className="flex-1 h-full w-full max-w-[900px]" />
--- src/components/quotations/index.tsx
+++ src/components/quotations/index.tsx
@@ -1,7 +1,21 @@
-const Quotations = () => {
+import { Row } from "../layout"
+
+interface Props<T>{
+      showQuotes: boolean,
+	list: Array<T>;
+	renderItem: (params: {
+		index: number;
+		item: T;
+	}) => React.JSX.Element | null;
+}
+export const Quotations = <T,>(props: Props<T>) => {
+      const {showQuotes, list, renderItem} = props
+      if(!showQuotes) return null;
       return (
-            <div>
-                  <p>Quotations</p>
-            </div>
+            <Row className="flex-wrap gap-4 w-full">
+                  {
+                        list.map((item, index) => renderItem({ index, item }))
+                  }
+            </Row>
       )
 }
