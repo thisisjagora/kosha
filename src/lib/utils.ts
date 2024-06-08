fd69ed363f6a24e52525d46c89a6275fb343cdbe
nelsonmic <iamnelsonmichael@gmail.com>
2024-06-08 17:28:30 +0100

    wip: added general quotations + quotes comps

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const truncateWithEllipsis = (text: string, maxLength: number) => {
+  if (typeof text !== 'string') throw new TypeError('Expected a string as the first argument');
+  if (typeof maxLength !== 'number' || maxLength < 0) throw new TypeError('Expected a non-negative number as the second argument');
+
+  if (text.length > maxLength) return text.slice(0, maxLength - 3) + '...';
+  return text;
+}
