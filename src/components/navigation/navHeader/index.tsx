eaca556f45cb7d6f96f919933773f235ad5b3d9f
nelsonmic <iamnelsonmichael@gmail.com>
2024-06-25 00:45:06 +0100

    wip: added account deletion

--- src/components/navigation/navHeader/index.tsx
+++ src/components/navigation/navHeader/index.tsx
@@ -3,11 +3,14 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/avatar";
 import { Column, Row } from "@/components/layout";
 import { Notification } from "@/components/notification";
 import { Routes } from "@/core/routing"
+import { generateAcronym } from "@/lib/helpers/generateAcronym";
 import useShowQuotes from "@/stores/show-quotes.store";
+import useUserStore from "@/stores/user.store";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export const NavHeader = () => {
+      const {user} =  useUserStore((state) => state);
       const path = usePathname();
       const showQuote = useShowQuotes((state) => state.showQuote);
       const [headerContent, setHeaderContent] = useState({ title: "", description: "" });
@@ -25,8 +28,8 @@ export const NavHeader = () => {
                   <Row className="flex-1 bg-white-100 p-4 items-center justify-between max-w-[120px] max-h-[60px] rounded-[30px]">
                         <Notification />
                         <Avatar className="w-[40px] h-[40px] bg-[#F6DF9C]">
-                              <AvatarImage src="https://images.unsplash.com/photo-1715005881129-266ccdd75e43?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Nelson Michael"/>
-                              <AvatarFallback>WC</AvatarFallback>
+                            <AvatarImage src={ user?.photoURL || "https://images.unsplash.com/photo-1715005881129-266ccdd75e43?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt={(user?.fullName || user?.displayName) ?? ""}/>
+                            <AvatarFallback>{generateAcronym((user?.fullName || user?.displayName) ?? "")}</AvatarFallback>
                         </Avatar>
                   </Row>
             </Row>
