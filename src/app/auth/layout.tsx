ae15af65a5507498b016c453634d35af13469f3d
nelsonmic <iamnelsonmichael@gmail.com>
2024-06-28 15:32:01 +0100

    fix: auth screen responsiveness

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,13 +1,14 @@
+import { Row } from "@/components/layout";
 import { FC, PropsWithChildren } from "react";
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
       return (
-            <div className="flex h-full pb-24">
+            <Row className="flex justify-center items-center h-full pb-24">
                   {children}
-                  <div className="flex-1">
+                  <div className="flex-1 hidden md:block">
 
                   </div>
-            </div>
+            </Row>
       )
 }
 
