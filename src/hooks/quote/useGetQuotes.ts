449c42fb717dbc5f56316ad6395a6a1a6906d021
nelsonmic <iamnelsonmichael@gmail.com>
2024-07-13 13:54:16 +0100

    feat: auto move user to quotes after sending requests

--- src/hooks/quote/useGetQuotes.ts
+++ src/hooks/quote/useGetQuotes.ts
@@ -5,45 +5,57 @@ import bookMoveStore from "@/stores/book-move.store";
 import useShowQuotes from "@/stores/show-quotes.store";
 import { BookMoveDto } from "@/types/dtos";
 import { Quote } from "@/types/structs";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, type UseMutationOptions } from "@tanstack/react-query";
 import { useValidRoute } from "../useValidRoute";
 import { Routes } from "@/core/routing";
 import hireLabourStore from "@/stores/hire-labour.store";
 
-export const useGetQuotes = () => {
-	const { isValidRoute: isHireLabourRoute } = useValidRoute(Routes.sequence.hireLabour)
-	// const { formData, reset } = useBookMoveStore((state) => state);
-	const setQuotesResult = useShowQuotes((state) => state.setQuotesResult);
-	const methods = useMutation<any, any, Partial<BookMoveDto>>({
-		mutationFn: (props) => getQuotesData(props)
-	    });
+export const useGetQuotes = (
+  useMutationOptions: Omit<
+    UseMutationOptions<any, any, Partial<BookMoveDto>>,
+    "mutationFn"
+  > = {}
+) => {
+  const { isValidRoute: isHireLabourRoute } = useValidRoute(
+    Routes.sequence.hireLabour
+  );
+  // const { formData, reset } = useBookMoveStore((state) => state);
+  const setQuotesResult = useShowQuotes((state) => state.setQuotesResult);
+  const methods = useMutation<any, any, Partial<BookMoveDto>>({
+    mutationFn: (props) => getQuotesData(props),
+    ...useMutationOptions,
+  });
 
-	const _useGetQuotes = (payload : Partial<BookMoveDto> ) =>
-		methods
-			.mutateAsync(payload)
-			.then((res) => {
-				const { formData, reset } = bookMoveStore.getState();
-				const {formData: hireLabourFormData, reset: resetHireLabour } = hireLabourStore.getState();
-				if(isHireLabourRoute){
-					localStorage.setItem(StorageKeys.FORM_DATA, JSON.stringify(hireLabourFormData))
-					setQuotesResult(res.result as Array<Quote>)
-					resetHireLabour()
-				}else{
-					localStorage.setItem(StorageKeys.FORM_DATA, JSON.stringify(formData))
-					setQuotesResult(res.result as Array<Quote>)
-					reset()
-				}
-			})
-			.catch(() => {
-                        toast({
-                              title:"Oops!",
-                              description: ErrorMessage.UNEXPECTED_ERROR, //TODO:add a proper function to handle error
-                              variant:"destructive"
-                        })
-			});
+  const _useGetQuotes = (payload: Partial<BookMoveDto>) =>
+    methods
+      .mutateAsync(payload)
+      .then((res) => {
+        const { formData, reset } = bookMoveStore.getState();
+        const { formData: hireLabourFormData, reset: resetHireLabour } =
+          hireLabourStore.getState();
+        if (isHireLabourRoute) {
+          localStorage.setItem(
+            StorageKeys.FORM_DATA,
+            JSON.stringify(hireLabourFormData)
+          );
+          setQuotesResult(res.result as Array<Quote>);
+          resetHireLabour();
+        } else {
+          localStorage.setItem(StorageKeys.FORM_DATA, JSON.stringify(formData));
+          setQuotesResult(res.result as Array<Quote>);
+          reset();
+        }
+      })
+      .catch(() => {
+        toast({
+          title: "Oops!",
+          description: ErrorMessage.UNEXPECTED_ERROR, //TODO:add a proper function to handle error
+          variant: "destructive",
+        });
+      });
 
-	return {
-		...methods,
-		getQuotes: _useGetQuotes
-	};
-}
+  return {
+    ...methods,
+    getQuotes: _useGetQuotes,
+  };
+};
