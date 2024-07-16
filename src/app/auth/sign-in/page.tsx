ee6b40180b29fe0d2060498e96abf46259ff6fe6
nelsonmic <iamnelsonmichael@gmail.com>
2024-07-16 14:03:04 +0100

    fix: take unauth user back to where they came from

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,49 +1,57 @@
-"use client"
+"use client";
 import { Caret, Google } from "@/components/Icons";
 import { Button, H, P } from "@/components/atoms";
 import { SignInForm } from "@/components/forms";
 import { Column, Row } from "@/components/layout";
 import { useSignInWithGoogle } from "@/hooks/auth/useSignIn";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
-      const {signInWithGoogle, loading} = useSignInWithGoogle()
-      return (
-            <div className="flex-1 flex items-center justify-center h-full">
-                  <Column className="justify-between gap-10 h-full w-full max-w-[420px]">
-                        <Link href="/">
-                              <Row className="text-primary-foreground font-dm-sans max-w-max p-2 items-center gap-4">
-                                    <Caret width={22} height={22}/>
-                                    <p className="">Back to Website</p>
-                              </Row>
-                        </Link>
-                        <Row className="flex-1 items-center">
-                              <Column className="gap-10 w-full">
-                                    <Column className="">
-                                          <H level={1} className="text-primary m-0 text-4xl">Sign In</H>
-                                          <P className="text-primary-foreground m-0 text-base font-dm-sans">Enter your email and password to sign in!</P>
-                                    </Column>
-                                    <Button
-                                          onClick={signInWithGoogle} 
-                                          disabled={loading}   
-                                          variant="ghost" 
-                                          size="lg"
-                                    >
-                                          <Google className="mr-2 h-[20px] w-[19px]" />
-                                          <p className="text-primary text-base">Sign in with Google</p>
-                                    </Button>
-                                    <Column className="gap-12">
-                                          <div className="relative">
-                                                <div className="border" />
-                                                <P className="font-dm-sans text-grey-100 p-2 bg-white-100 max-w-max absolute left-[50%] -bottom-4 translate-x-[-50%]">or</P>
-                                          </div>
-                                          <SignInForm />
-                                    </Column>
-                              </Column>
-                        </Row>
-                  </Column>
-            </div>
-      )
-}
+  const { signInWithGoogle, loading } = useSignInWithGoogle();
+  const router = useRouter();
+  return (
+    <div className="flex-1 flex items-center justify-center h-full">
+      <Column className="justify-between gap-10 h-full w-full max-w-[420px]">
+        <Row
+          className="text-primary-foreground font-dm-sans max-w-max p-2 items-center gap-4 cursor-pointer"
+          onClick={() => router.back()}
+        >
+          <Caret width={22} height={22} />
+          <p className="">Back to Website</p>
+        </Row>
+        <Row className="flex-1 items-center">
+          <Column className="gap-10 w-full">
+            <Column className="">
+              <H level={1} className="text-primary m-0 text-4xl">
+                Sign In
+              </H>
+              <P className="text-primary-foreground m-0 text-base font-dm-sans">
+                Enter your email and password to sign in!
+              </P>
+            </Column>
+            <Button
+              onClick={signInWithGoogle}
+              disabled={loading}
+              variant="ghost"
+              size="lg"
+            >
+              <Google className="mr-2 h-[20px] w-[19px]" />
+              <p className="text-primary text-base">Sign in with Google</p>
+            </Button>
+            <Column className="gap-12">
+              <div className="relative">
+                <div className="border" />
+                <P className="font-dm-sans text-grey-100 p-2 bg-white-100 max-w-max absolute left-[50%] -bottom-4 translate-x-[-50%]">
+                  or
+                </P>
+              </div>
+              <SignInForm />
+            </Column>
+          </Column>
+        </Row>
+      </Column>
+    </div>
+  );
+};
 
 export default Page;
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,22 +3,22 @@ import { onAuthStateChanged } from "firebase/auth";
 import { Routes } from "./core/routing";
 import { auth } from "./firebase/auth";
 
-
 export const middleware = async (request: NextRequest) => {
-	// const { root, profile, messages, bookings, license, terms, sequence } = Routes;
-	// const authRoutes = [root, profile, messages, bookings, license, terms, ...Object.values(sequence)];
-	const { profile, messages, bookings, license, terms } = Routes;
-	const authRoutes = [profile, messages, bookings, license, terms];
+  // const { root, profile, messages, bookings, license, terms, sequence } = Routes;
+  // const authRoutes = [root, profile, messages, bookings, license, terms, ...Object.values(sequence)];
+  const { profile, messages, bookings, license, terms } = Routes;
+  const authRoutes = [profile, messages, bookings, license, terms];
 
-      onAuthStateChanged(auth, (user) => {
-            if (authRoutes.includes(request.nextUrl.pathname)) {
-                  if (!user)
-                        return NextResponse.redirect(new URL(Routes.signIn, request.url));
-            }
-      })
-	return NextResponse.next();
+  onAuthStateChanged(auth, (user) => {
+    if (authRoutes.includes(request.nextUrl.pathname)) {
+      if (!user) {
+        return NextResponse.redirect(new URL(Routes.signIn, request.url));
+      }
+    }
+  });
+  return NextResponse.next();
 };
 
 export const config = {
-	matcher: "/((?!api|static|.*\\..*|_next).*)"
+  matcher: "/((?!api|static|.*\\..*|_next).*)",
 };
