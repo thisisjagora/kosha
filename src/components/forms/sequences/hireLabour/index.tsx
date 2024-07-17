26c90b07c3df6dcf4e8976abf707d91b802577d5
nelsonmic <iamnelsonmichael@gmail.com>
2024-07-17 22:50:37 +0100

    fix: update building type options for hire labour

--- src/components/forms/sequences/hireLabour/index.tsx
+++ src/components/forms/sequences/hireLabour/index.tsx
@@ -186,7 +186,11 @@ const Step1: FC<SequenceStepsProps> = ({ onChangeStep }) => {
                     <SelectContent>
                       <SelectItem value="Condo">Condo</SelectItem>
                       <SelectItem value="Apartment">Apartment</SelectItem>
-                      <SelectItem value="Penthouse">Penthouse</SelectItem>
+                      <SelectItem value="House">House</SelectItem>
+                      <SelectItem value="Office">Office</SelectItem>
+                      <SelectItem value="TownHouse">TownHouse</SelectItem>
+                      <SelectItem value="Storage">Storage</SelectItem>
+                      <SelectItem value="Store">Store</SelectItem>
                     </SelectContent>
                   </Select>
                   <FormMessage className="text-destructive" />
